b82466bd40e45fddf7896f2f67bb2733a107d9e3
Divya Mali <162532947+malidivya184@users.noreply.github.com>
2026-04-14 11:28:28 +0530

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,12 @@
+import json
+
+def lambda_handler(event, context):
+    return {
+        'statusCode': 200,
+        'headers': {
+            "Access-Control-Allow-Origin": "*"
+        },
+        'body': json.dumps({
+            "message": "Welcome to Marathi Matrimony API 💍"
+        })
+    }
